43c6c9b8c5744edaaaebb10151b704d1ae6dbbb1
Jonathan Cordero <cryptcur00101@protonmail.com>
2020-06-20 19:58:55 -0400

    yelp api fixed

--- client/hooks/findRestaurants.js
+++ client/hooks/findRestaurants.js
@@ -1,28 +1,24 @@
-import {useState, useEffect} from 'react'
-// import yelp from '../../server/api/yelp'
+import {useState} from 'react'
+import axios from 'axios'
 
 export default () => {
   const [restaurants, setRestaurants] = useState([])
   const [error, setError] = useState('')
 
   const apiSearch = async search => {
-    // try {
-    //   const res = await yelp.get('/search', {
-    //     params: {
-    //       limit: search.limit || 10,
-    //       categories: search.categories || '',
-    //       latitude: search.latitude || 0,
-    //       longitude: search.longitude || 0,
-    //       radius: 1500
-    //     }
-    //   })
-    //   setRestaurants(res.data.businesses)
-    //   return res.data.businesses
-    // } catch (ex) {
-    //   console.error(ex, 'ERROR IN YELP')
-    //   setError('Something went wrong')
-    // }
-    return null
+    try {
+      const response = await axios.post('/api/yelp', {
+        limit: search.limit || 10,
+        categories: search.categories || '',
+        latitude: search.latitude || 0,
+        longitude: search.longitude || 0,
+        radius: 1500
+      })
+      setRestaurants(response.data)
+    } catch (ex) {
+      console.error('Error', ex)
+      setError('Something went wrong requesting restaurants', ex)
+    }
   }
 
   return [apiSearch, restaurants, error]
--- server/api/yelp.js
+++ server/api/yelp.js
@@ -3,16 +3,10 @@ const router = require('express').Router()
 const {yelpKey} = require('../../secrets')
 const apiKey = process.env.YELP_API_KEY || yelpKey
 const axios = require('axios')
+// const {Restaurant} = require('../db/models')
 module.exports = router
 
-// const yelp = axios.create({
-//   baseURL: `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses`,
-//   headers: {
-//     Authorization: `Bearer ${apiKey}`
-//   }
-// })
-router.get('/', async (req, res, next) => {
-  console.log('YELP!!!!!!!!!')
+router.post('/', async (req, res, next) => {
   try {
     const output = await axios.get(
       `https://api.yelp.com/v3/businesses/search`,
@@ -20,41 +14,17 @@ router.get('/', async (req, res, next) => {
         headers: {
           Authorization: `Bearer ${apiKey}`
         },
-        //pass in the req.body
         params: {
-          limit: 3,
-          location: 'new york',
-          term: 'pasta'
+          limit: req.body.limit,
+          categories: req.body.categories,
+          latitude: req.body.latitude,
+          longitutde: req.body.longitude,
+          radius: req.body.radius
         }
       }
     )
-    console.log('OUTPUT', output.data.businesses)
-
     res.send(output.data.businesses)
   } catch (ex) {
     next(ex)
   }
 })
-
-// axios.get('/search', (req, res, next) => {
-//   const apiSearch = async search => {
-//     try {
-//       const res = await yelp.get('/search', {
-//         params: {
-//           limit: search.limit || 10,
-//           categories: search.categories || '',
-//           latitude: search.latitude || 0,
-//           longitude: search.longitude || 0,
-//           radius: 1500
-//         }
-//       })
-//       setRestaurants(res.data.businesses)
-//       return res.data.businesses
-//     } catch (ex) {
-//       console.error(ex, 'ERROR IN YELP')
-//       setError('Something went wrong')
-//     }
-//   }
-// })
-
-// export default yelp
