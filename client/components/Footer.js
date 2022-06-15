d5d67b6ff5c7722eeed131959ab02cfb0396d8d0
Jonathan <jncrdro@gmail.com>
2022-06-15 13:49:41 -0400

    changes

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -7,25 +7,25 @@ export default function Footer() {
     <div className="footer">
         <div className='footerPerson darkFontMedium'>
           <span className='footerName'>Andres Callanaupa</span>
-          <a className='darkFontSmall' href="https://www.linkedin.com/in/andrescallanaupa/">LinkedIn</a>
+          {/* <a className='darkFontSmall' href="https://www.linkedin.com/in/andrescallanaupa/">LinkedIn</a> */}
           <a className='darkFontSmall' href="https://github.com/avc278">Github</a>
         </div>
 
         <div className='footerPerson darkFontMedium'>
           <span className='footerName'>Diana Aguilar</span>
-          <a className='darkFontSmall' href="https://www.linkedin.com/in/diana-aguilar/">LinkedIn</a>
+          {/* <a className='darkFontSmall' href="https://www.linkedin.com/in/diana-aguilar/">LinkedIn</a> */}
           <a className='darkFontSmall' href="https://github.com/DianaCAguilar">Github</a>
         </div>
 
         <div className='footerPerson darkFontMedium'>
           <span className='footerName'>Jonathan Cordero</span>
-          <a className='darkFontSmall' href="https://www.linkedin.com/in/jonathancordero7/">LinkedIn</a>
+          {/* <a className='darkFontSmall' href="https://www.linkedin.com/in/jonathancordero7/">LinkedIn</a> */}
           <a className='darkFontSmall' href="https://github.com/Cryptcur">Github</a>
         </div>
 
         <div className='footerPerson darkFontMedium'>
           <span className='footerName'>Mike Olsen</span>
-          <a className='darkFontSmall' href="https://www.linkedin.com/in/mikeo3d/">LinkedIn</a>
+          {/* <a className='darkFontSmall' href="https://www.linkedin.com/in/mikeo3d/">LinkedIn</a> */}
           <a className='darkFontSmall' href="https://github.com/olsen3d">Github</a>
         </div>
     </div>
